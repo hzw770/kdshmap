dd49c5227fc13cd958053e42b694c710796f202f
Ziwen Huang <ziwenh@Mac.hsd1.il.comcast.net>
2025-03-12 16:50:48 -0500

    setup.py update

--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@ setup(
     name='kdshmap',
     version='0.1',
     description='Python package for calculating Keldysh dynamical maps for open quantum systems',
-    author='Ziwen Huang',
+    author='Ziwen Huang, Rohan Rajmohan',
     author_email='zhuang@fnal.gov',
     packages=['kdshmap', 'kdshmap.core', 'kdshmap.utils', 'kdshmap.tests'],
     install_requires=[
-        'cython>=0.29.20,<3.0.0',
-        'matplotlib>=3.5',
-        'numpy>=1.14.2',
-        'qutip>=4.3.1',
-        'scipy>=1.1.0',
-        'pathos>=0.3.1'
+        'cython>=3.0.12',
+        'matplotlib>=3.10.1',
+        'contourpy>=1.3.1',
+        'numpy>=2.2.3',
+        'qutip>=5.1.1',
+        'scipy>=1.15.2',
+        'pathos>=0.3.3'
     ]
 )
